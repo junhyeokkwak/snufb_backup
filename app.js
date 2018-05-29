b3b28fd4910b2be5f4efc3695e3d239c49413392
Trevor Shim <trevorshim@outlook.kr>
2018-05-28 23:02:36 -0400

    ejs module added

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ app.post('/webhook', function (req, res) {
 
 app.get('/mentor-admin', function(req, res) {
   GetData(function (recordSet) {
-        res.render('mentor', {title: "title", recordSet: recordset });
+        res.render('mentor', {title: "title", recordSet: recordSet });
         console.log(recordSet);
     });
 });
