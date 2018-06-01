43d0d471f76382b6948847b6e83e3894d83f973b
Trevor Shim <trevorshim@outlook.kr>
2018-05-31 23:36:45 -0400

    bab

--- bab.js
+++ bab.js
@@ -1,36 +1,45 @@
 var request = require("request");
 var api = require("./apiCalls");
+var async = require("async");
 
 //give out list of sikdangs
 var whichSikdang = function(event){
-  var sikdang = [];
   var utc = new Date().setUTCHours(28);
   var todayDate = new Date(utc).toISOString().slice(0,10);
   var key = "I5mnxs3t4W";
-  var options = { method: 'GET',
-      url: 'https://bds.bablabs.com/openapi/v1/campuses/' + key + '/stores',
-      qs: { date: todayDate },
-      headers:
-       { 'postman-token': '13d05bcc-6df0-81ab-df78-180ddeafbeee',
-         'cache-control': 'no-cache',
-         babsession: '123',
-         accesstoken: 'O1t5rnRk80LEErp1NIPgwSy1Inz0xOCtITLovskaYckJohmwsV' } };
+  var task = [
+    function (callback){
+      var sikdang = [];
+      var options = { method: 'GET',
+          url: 'https://bds.bablabs.com/openapi/v1/campuses/' + key + '/stores',
+          qs: { date: todayDate },
+          headers:
+           { 'postman-token': '13d05bcc-6df0-81ab-df78-180ddeafbeee',
+             'cache-control': 'no-cache',
+             babsession: '123',
+             accesstoken: 'O1t5rnRk80LEErp1NIPgwSy1Inz0xOCtITLovskaYckJohmwsV' } };
 
-    request(options, function (error, response, body) {
-      if (error) throw new Error(error);
-      //JSON.parse(body).stores.length
-      for (i = 0; i < 3; i++){
-        sikdang.push({
-          "content_type": "text",
-          "title": JSON.parse(body).stores[i].name,
-          "payload": JSON.parse(body).stores[i].name
+        request(options, function (error, response, body) {
+          if (error) throw new Error(error);
+          //JSON.parse(body).stores.length
+          for (i = 0; i < 3; i++){
+            sikdang.push({
+              "content_type": "text",
+              "title": JSON.parse(body).stores[i].name,
+              "payload": JSON.parse(body).stores[i].name
+            });
+          }
         });
-      }
-    });
-    console.log(sikdang);
-
-    var messageData = {"text": "어디서 먹을건데?", "quick_replies": sikdang};
-    api.sendResponse(event, messageData);
+      callback(null, sikdang);
+    },
+    function (sikdang, callback){
+      console.log(sikdang);
+      var messageData = {"text": "어디서 먹을건데?", "quick_replies": sikdang};
+      api.sendResponse(event, messageData);
+      callback(null);
+    }
+  ]
+  async.waterfall(task);
 }
 
 var sendBabMenu = function(event){
