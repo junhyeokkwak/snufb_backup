caa8f3c0f934f3041bc8b4fdc2069862599d71aa
jaeleeps <jaeleeps@gmail.com>
2018-06-01 15:27:42 +0900

    restaurant init

--- functionSheet.js
+++ functionSheet.js
@@ -1,4 +1,4 @@
-﻿var fileNameList = ["./UserSetup.js", "./love.js", "./mentor.js", "./bab.js", "./restaurant.js", "./utilfunctions"];
+﻿var fileNameList = ["./UserSetup.js", "./love.js", "./mentor.js", "./bab.js", "./restaurant.js", "./utilfunctions.js"];
 var functionSheet = [];
 
 fileNameList.forEach(function (fileName) {
