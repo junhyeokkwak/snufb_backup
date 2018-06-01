3099cff75975b355f3bb39efec117cf2cd6e4c41
jaeleeps <jaeleeps@gmail.com>
2018-06-01 13:57:52 +0900

    restaurant init

--- functionSheet.js
+++
@@ -1,9 +0,0 @@
-﻿var fileNameList = ["./UserSetup.js", "./love.js", "./mentor.js", "./test.js"];
-var functionSheet = [];
-
-fileNameList.forEach(function (fileName) {
-    var funclist = require(fileName).functionMatch;
-    functionSheet = Object.assign(functionSheet, funclist);
-});
-
-module.exports = functionSheet;
