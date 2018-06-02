eecd54ca434096158c5c6d626bfaaf2af147bdf8
jaeleeps <jaeleeps@gmail.com>
2018-06-02 18:50:26 +0900

    restaurant v2

--- utilfunctions.js
+++ utilfunctions.js
@@ -22,39 +22,29 @@ function generateQuickReplies(arr){
   }
 };
 
-function getJosa(txt, josa)
-{
+function Josa(txt, josa){
 	var code = txt.charCodeAt(txt.length-1) - 44032;
 	var cho = 19, jung = 21, jong=28;
 	var i1, i2, code1, code2;
-	// empty str
-	if (txt.length == 0) {
-    console.log("TXT is not JOSA");
-    return '';
-  }
-  // !Korean
-	if (code < 0 || code > 11171) {
-    console.log("TXT is not JOSA");
-    return txt;
-  }
-  // Korean
-	if (code % 28 == 0) return txt + getJosaHelper(josa, false);
-	else return txt + getJosaHelper(josa, true);
+	// 원본 문구가 없을때는 빈 문자열 반환
+	if (txt.length == 0) return '';
+	// 한글이 아닐때
+	if (code < 0 || code > 11171) return txt;
+	if (code % 28 == 0) return txt + Josa.get(josa, false);
+	else return txt + Josa.get(josa, true);
 }
-module.exports.getJosa = getJosa;
 
-function getJosaHelper (josa, jong) {
+Josa.get = function (josa, jong) {
 	// jong : true면 받침있음, false면 받침없음
 	if (josa == '을' || josa == '를') return (jong?'을':'를');
 	if (josa == '이' || josa == '가') return (jong?'이':'가');
 	if (josa == '은' || josa == '는') return (jong?'은':'는');
 	if (josa == '와' || josa == '과') return (jong?'와':'과');
-	// Undefined 조사
+	// 알 수 없는 조사
 	return '**';
 }
-module.exports.getJosaHelper = getJosaHelper;
 
-
+module.exports.Josa = Josa;
 module.exports = {
     functionMatch: {
         "RESET" : reset,
