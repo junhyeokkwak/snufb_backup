b030e6c4c67b950ad032ca0333db66f5a25221ce
jaeleeps <jaeleeps@gmail.com>
2018-06-02 16:24:37 +0900

    restaurant v2

--- restaurant.js
+++ restaurant.js
@@ -93,10 +93,18 @@ var restaurantRecommendation_1 = function(event) {
         console.log(JSON.parse(body).items[0].title);
         console.log(JSON.parse(body).items[0].link);
         var title = JSON.parse(body).items[0].title;
-        var url = JSON.parse(body).items[0].link;
         var titleMessage = "오늘메뉴는 "+title+" 어때??:)";
+        var url = JSON.parse(body).items[0].link;
         var buttonMessage = title + " 홈페이지 바로가기!";
-        api.handleRestaurantWebview(event, titleMessage, url, buttonMessage)
+        if (url == '') {
+          console.log('RESTAURANT URL DNE');
+          url = 'http://www.example.com/'
+          api.handleRestaurantWebview(event, titleMessage, url, buttonMessage)
+        } else {
+          console.log('RESTAURANT URL EXISTS');
+          var buttonMessage = title + " 홈페이지 바로가기!";
+          api.handleRestaurantWebview(event, titleMessage, url, buttonMessage)
+        }
       });
       callback(null);
     },
