b303e24642ac685d35563ba49ff0e6c169bb610c
jaeleeps <jaeleeps@gmail.com>
2018-06-02 19:04:50 +0900

    restaurant v2

--- utilfunctions.js
+++ utilfunctions.js
@@ -1,6 +1,5 @@
 var request = require("request");
 var api = require("./apiCalls");
-var util = require('./utilfunctions');
 var async = require("async");
 var mysql = require("mysql");
 var connection = mysql.createConnection(process.env.DATABASE_URL);
@@ -31,18 +30,20 @@ function Josa(txt, josa){
 	if (txt.length == 0) return '';
 	// 한글이 아닐때
 	if (code < 0 || code > 11171) return txt;
-	if (code % 28 == 0) return txt + Josa.get(josa, false);
-	else return txt + Josa.get(josa, true);
-}
-
-Josa.get = function (josa, jong) {
-	// jong : true면 받침있음, false면 받침없음
-	if (josa == '을' || josa == '를') return (jong?'을':'를');
-	if (josa == '이' || josa == '가') return (jong?'이':'가');
-	if (josa == '은' || josa == '는') return (jong?'은':'는');
-	if (josa == '와' || josa == '과') return (jong?'와':'과');
-	// 알 수 없는 조사
-	return '**';
+  var jong
+	if (code % 28 == 0) {
+    if (josa == '을' || josa == '를') jong?'을':'를';
+    if (josa == '이' || josa == '가') jong?'이':'가';
+    if (josa == '은' || josa == '는') jong?'은':'는';
+    if (josa == '와' || josa == '과') jong?'와':'과';
+    return txt + Josa.get(josa, false);
+  } else {
+    if (josa == '을' || josa == '를') jong?'을':'를';
+    if (josa == '이' || josa == '가') jong?'이':'가';
+    if (josa == '은' || josa == '는') jong?'은':'는';
+    if (josa == '와' || josa == '과') jong?'와':'과';
+    return txt + Josa.get(josa, true);
+  }
 }
 
 module.exports.Josa = Josa;
