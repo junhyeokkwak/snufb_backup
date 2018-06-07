ba8ceb9ca291c733ba8319ac327b7ff31dc7278c
JunHyeok Kwak <starjun9123@gmail.com>
2018-06-07 14:47:05 +0900

    practice

--- UserSetup.js
+++ UserSetup.js
@@ -143,7 +143,7 @@ function register2(event) {
       connection.query('SELECT first_name FROM Users WHERE user_id=' + event.sender.id, function(err, result, fields) {
         if (err) throw err;
         //console.log("hi" + result);
-        api.sendResponse(event, {"text": result + "고마워!"}); // JSON.stringify 빼고 실험.
+        api.sendResponse(event, {"text": JSON.stringify(result) + "고마워!"}); // JSON.stringify 빼고 실험.
       });
       callback(null, 'done');
     },
