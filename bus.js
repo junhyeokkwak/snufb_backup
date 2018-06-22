5199d787280370b43bf001348e8428ab9bc19ff2
jaeleeps <jaeleeps@gmail.com>
2018-06-22 15:47:19 +0900

    bus string similarity

--- bus.js
+++ bus.js
@@ -140,7 +140,7 @@ var bus_askStNm = function(event) {
         console.log("BUSNUM that user chose:" + result[0].busNum);
         if (result[0].busNum != ("none" && "")) {
           // NOTE: if there is confirmed busNum, search only the stations which the bus go through
-          for (var i = 0, i < jsonData.busRouteId_stId_staOrd.length; i++) {
+          for (var i = 0; i < jsonData.busRouteId_stId_staOrd.length; i++) {
             if (jsonData.busRouteId_stId_staOrd[i].plainNo == result[0].busNum) {
               console.log(jsonData.busRouteId_stId_staOrd[i].stNm);
               stNameArr.push(jsonData.busRouteId_stId_staOrd[i].stNm);
