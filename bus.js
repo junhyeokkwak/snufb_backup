493d2811a9a4bc65a60bc998cb354ea1299e262e
jaeleeps <jaeleeps@gmail.com>
2018-06-25 18:08:43 +0900

    hihi

--- bus.js
+++ bus.js
@@ -237,7 +237,7 @@ var bus_confirmStNm = function(event) {
   async.waterfall(task);
 }
 
-var bus_handleMultipleStNm = function(event) {
+var bus_handleMultipleStNm = function(event, possibleStArr) {
   console.log("RUN handleMultipleStNm");
   console.log("possibleStArr: " + JSON.stringify(possibleStArr));
   var title = "TEST";
