2e15fdf470cf5fbaccf438ff6a7bf57f8dcff034
JunHyeok Kwak <starjun9123@gmail.com>
2018-06-25 17:06:32 +0900

    two text experiment

--- personSearch.js
+++ personSearch.js
@@ -18,6 +18,8 @@ var startPersonSearch = function(event) {
         if (result[0].uid == 0)
         {
           console.log('Need to ask for profile URL');
+          connection.query('UPDATE Users SET conv_context="askProfileURL" WHERE user_id=' + event.sender.id);
+          api.sendResponse(event, {"text": "이 기능을 처음 사용하는구나! 먼저 너 프로필을 등록해야 하는데 이건 너의 도움이 조금 필요해!", "text": "도와줄수 있지?"});
         };
         callback(null,'done');
       })
@@ -33,5 +35,6 @@ var startPersonSearch = function(event) {
 module.exports = {
   functionMatch: {
     "사람찾기": startPersonSearch,
+    "askProfileURL": askProfileURL,
   }
 };
