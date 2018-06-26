0f03e8bcf86ccb62a94163e7c437f56271998a31
jaeleeps <jaeleeps@gmail.com>
2018-06-26 13:45:27 +0900

    hihi

--- webviews/busRouteWebview.js
+++ webviews/busRouteWebview.js
@@ -13,7 +13,7 @@
 // });
 alert("sex");
 
-document.querySelector('.ajaxsend').addEventListener('click', function(){
+document.getElementById('ajaxsend').addEventListener('click', function(){
 // 입력값 위치를 찾아 변수에 담고
 var inputdata = document.forms[0].elements[0].value;
 // sendAjax 함수를 만들고 URL과 data를 전달
