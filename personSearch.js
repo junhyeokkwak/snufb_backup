b5037527995d6a78591feaa9349fd1bc6e15ecc8
JunHyeok Kwak <starjun9123@gmail.com>
2018-07-05 15:24:29 +0900

    personSearch webview

--- personSearch.js
+++ personSearch.js
@@ -177,9 +177,10 @@ function personSearch_alum(event) {
             api.sendResponse(event, {"text": "이 링크를 누르면 직접 페메를 보낼 수 있어!\nm.me/" + uid});
             var title = "이대봇의 친구를 소개합니다";
             var url = "https://m.me/" + uid;
+            var profileURL = "https://www.facebook.com/" + uid;
             // api.handlePersonSearchWebview(event, title, url, uid);
-            api.handleWebview(event, title, url, "compact");
-            api.handlePersonSearchWebview(event, title, url, uid);
+            api.handleWebview(event, title, profileURL, "compact");
+            // api.handlePersonSearchWebview(event, title, url, uid);
         }
         callback(null, 'done');
       }
