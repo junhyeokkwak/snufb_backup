c13efc61d11b01ce0e597e8054a7062705ddae0f
jaeleeps <jaeleeps@gmail.com>
2018-07-05 16:47:17 +0900

    restaurant v3

--- restaurant.js
+++ restaurant.js
@@ -152,8 +152,7 @@ var restaurantRecommendation_freeResponse = function(event) {
               "image_url" : 'https://scontent-icn1-1.xx.fbcdn.net/v/t1.0-9/34644235_2070034323285218_6642764812776374272_n.jpg?_nc_cat=0&oh=e28acdba08325a59a83582152e071b54&oe=5BC084EE',
               // "image_url" : image_url,
               "title": jsonRestaurantData.results[i].name,
-              "subtitle" : `주소: ${jsonRestaurantData.results[i].vicinity} / 평점: ${jsonRestaurantData.results[i].rating}`,
-
+              // "subtitle" : `주소: ${jsonRestaurantData.results[i].vicinity} / 평점: ${jsonRestaurantData.results[i].rating}`,
               "buttons": [
                 {
                   "title":`${jsonRestaurantData.results[i].name} 위치보기!`,
@@ -165,7 +164,8 @@ var restaurantRecommendation_freeResponse = function(event) {
               ],
             }//template
           )//push
-          if (i == (jsonRestaurantData.results.length || 10)) {
+          if (i == (jsonRestaurantData.results.length-1 || 9)) {
+            console.log("SENDING genericTemplatesArr");
             var messageData = {
               "recipient":{
                 "id":event.sender.id
