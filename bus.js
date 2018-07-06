6284d36414e2455af43c54e55566809a55e31696
jaeleeps <jaeleeps@gmail.com>
2018-07-06 17:17:47 +0900

    restaurant google places nearbysearch API

--- bus.js
+++ bus.js
@@ -307,8 +307,8 @@ var bus_handleMultipleStNm = function(event, targetStNm, possibleStArr, callback
 
   var bus_busRouteWebviewHelper = function(event, responseData) {
     console.log('RUN bus_busRouteWebviewHelper1');
-    app.APP.get(`/busRoute/targetStNm/${event.sender.id}`, function(req, res){
-      res.render(__dirname + '/webviews/multipleBusStNmWebview.html', responseData);
+    app.APP.get(`/busRoute/${targetStNm}/${event.sender.id}`, function(req, res){
+      res.render(__dirname + '/webviews/multipleBusStNmWebview.html', {positionData: responseData});
     });
   }
 
