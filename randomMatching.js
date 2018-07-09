053468d192eb3f76db83aff45e5529f547221c70
JunHyeok Kwak <starjun9123@gmail.com>
2018-07-09 15:47:02 +0900

    randomMatching beta

--- randomMatching.js
+++ randomMatching.js
@@ -108,7 +108,7 @@ function randomMatching_gender(event) {
   var inputText = event.message.text;
   var substring1 = "학과";
   console.log("typeof:::::"  + typeof inputText);
-  console.log("equal to NAMJA???? " + inputText == "남자");
+  console.log("equal to NAMJA???? " + inputText == '남자');
   if (inputText !== "남자" || inputText !== "여자" || inputText !== "상관없어")
   {
     api.sendResponse(event, {"text": "엥 뭔가 잘못친거 같은데... 다시 입력해줄래?", "quick_replies": qr.reply_arrays["genderOptions"]});
