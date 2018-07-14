969ba5e1bcdb02a23821ae0bfec710221f75d495
JunHyeok Kwak <starjun9123@gmail.com>
2018-07-14 14:25:23 +0900

    UserSetup edit JHK 0714: natural flow + images

--- app.js
+++ app.js
@@ -113,6 +113,7 @@ app.post('/webhook', function (req, res) {
                 });
                 apiaiSession.on('response', function(response) {
                   //console.log(functionSheet[event.message.text])
+                  console.log(stringSimilarity.findBestMatch(event.message.text, functionSheet));
                   callback(null, (functionSheet[event.message.text] || functionSheet[response.result.metadata.intentName] || functionSheet["구구야!"] || functionSheet["fallback"]));
                 });
                 apiaiSession.on('error', function(error) {
