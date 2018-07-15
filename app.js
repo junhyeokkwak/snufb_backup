c4e86495b2e87057dc5c47665f97f181610b5de4
jaeleeps <jaeleeps@gmail.com>
2018-07-15 19:16:25 +0900

    bus DF intent/entity

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ app.post('/webhook', function (req, res) {
                   console.log("Closest function is: " + closestFunction._text);
                   console.log("IntentName is: " + response.result.metadata.intentName);
                   if (response.result.metadata.intentName == "initBusConv") {
-                    console.log("BUS parameters: " + JSON.stringify(response.result.metadata));
+                    console.log("BUS parameters: " + JSON.stringify(response.result));
                   }
                   callback(null, (functionSheet[event.message.text] || functionSheet[closestFunction] || functionSheet[response.result.metadata.intentName] || functionSheet["구구야!"] || functionSheet["fallback"]));
                 });
